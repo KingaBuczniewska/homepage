680c0436cc102189a2fec43913c169eb66199e43
Kinga Buczniewska <kingabuczniewska@gmail.com>
2021-05-04 23:26:41 +0200

    add function

--- js/script.js
+++ js/script.js
@@ -1,26 +1,53 @@
+{
+    
+    const showImage = () => {
 
-let header__button = document.querySelector(".header__button");
-let header__image = document.querySelector(".header__image");
+       const header__image = document.querySelector(".header__image");
 
-header__button.addEventListener("click", () => {
-    if (header__image.classList.contains("hidden")) { // czy jest niewidzialny
-        header__image.classList.remove("hidden"); // usun niewidzialnosc
-        header__button.innerText = "Schowaj zdjęcie";
-    } else {
-        header__image.classList.add("hidden"); // dodaj niewidzialnosc
-        header__button.innerText = "Pokaż zdjęcie";
+        if (header__image.classList.contains("hidden")) { // czy jest niewidzialny
+            header__image.classList.remove("hidden"); // usun niewidzialnosc
+            header__button.innerText = "Schowaj zdjęcie";
+
+        } else {
+            header__image.classList.add("hidden"); // dodaj niewidzialnosc
+            header__button.innerText = "Pokaż zdjęcie";
+        }
+
     }
 
-
-    console.log(button.innerText)
-});
-
+    clickButton();
+};
 
 
+const init = () => {
+    header__button.addEventListener("click");
+     const header__button = document.querySelector(".header__button");
+      
+};
 
-/*
+init();
 
 
+
+
+
+
+
+ //let header__button = document.querySelector(".header__button");
+// let header__image = document.querySelector(".header__image");
+
+// header__button.addEventListener("click", () => {
+//     if (header__image.classList.contains("hidden")) { // czy jest niewidzialny
+//         header__image.classList.remove("hidden"); // usun niewidzialnosc
+//         header__button.innerText = "Schowaj zdjęcie";
+//     } else {
+//         header__image.classList.add("hidden"); // dodaj niewidzialnosc
+//         header__button.innerText = "Pokaż zdjęcie";
+//     }
+
+
+//     console.log(button.innerText)
+// });
 
 
 // if(button.innerText === "Pokaż zdjęcie") {
@@ -32,23 +59,18 @@ header__button.addEventListener("click", () => {
 // }
 //    img1.remove();
 
-*/
 
+            // Co to ClassList?
 
-/*
-            Co to ClassList?
+            // Jesli mamy cos takiego:
+            //  <div class="raz dwa trzy"/>
 
-            Jesli mamy cos takiego:
-             <div class="raz dwa trzy"/>
+            // To ClassList tego diva zawiera 3 elementy:
+            // raz
+            // dwa
+            // trzy
 
-            To ClassList tego diva zawiera 3 elementy:
-            raz
-            dwa
-            trzy
-
-            Na ClassList mozesz wykonywac operacje:
-            ClassList.contains(costam) - sprawdza czy jest costam
-            ClassList.add(costam) - dodaje costam
-            ClassList.remove(costam) - usuwa costam
-
-*/
+            // Na ClassList mozesz wykonywac operacje:
+            // ClassList.contains(costam) - sprawdza czy jest costam
+            // ClassList.add(costam) - dodaje costam
+            // ClassList.remove(costam) - usuwa costam
